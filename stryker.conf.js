2c1ba7c26f67624cd809b87f97b584186b073f12
Ihor <tsake.love@gmail.com>
2020-12-04 00:06:04 +0200

    new tests

--- stryker.conf.js
+++ stryker.conf.js
@@ -4,7 +4,21 @@
 module.exports = {
   packageManager: "npm",
   reporters: ["html", "clear-text", "progress"],
+  mutator: "javascript",
   testRunner: "jest",
-  coverageAnalysis: "off"
+  coverageAnalysis: "off",
+  mutate: ["src/service/UserService.js", "src/service/ProducerService.js","src/service/OrderService.js"]
 };
-
+// module.exports = function(config) {
+//   config.set({
+//     files: [{
+//       pattern: 'app.js',
+//       mutated: true
+//     },
+//       'test/**/*.js'
+//     ],
+//     testRunner: 'mocha',
+//     reporter: ['html', 'clear-text', 'progress'],
+//     testFramework: 'mocha'
+//   });
+// };
